83d10273b03e8f09ac2ac7ba361a75d65e966279
Colin Sullivan <colin.sullivan@apcera.com>
2017-01-21 15:05:08 -0700

    Add collector_test.go

---
+++ collector/collector_test.go
@@ -0,0 +1,103 @@
+// Copyright 2016 Apcera Inc. All rights reserved.
+
+package collector
+
+import (
+	"fmt"
+	"strings"
+	"testing"
+	"time"
+
+	pet "github.com/nats-io/prometheus-nats-exporter/test"
+	"github.com/prometheus/client_golang/prometheus"
+	dto "github.com/prometheus/client_model/go"
+)
+
+func TestVarz(t *testing.T) {
+	s := pet.RunServer()
+	defer s.Shutdown()
+
+	url := fmt.Sprintf("http://localhost:%d/", pet.MonitorPort)
+
+	nc := pet.CreateClientConnSubscribeAndPublish(t)
+	defer nc.Close()
+
+	// see if we get the same stats as the original monitor testing code.
+	// just for our monitoring_port
+
+	cases := map[string]float64{
+		"gnatsd_varz_total_connections": 2,
+		"gnatsd_varz_connections":       1,
+		"gnatsd_varz_in_msgs":           1,
+		"gnatsd_varz_out_msgs":          1,
+		"gnatsd_varz_in_bytes":          5,
+		"gnatsd_varz_out_bytes":         5,
+		"gnatsd_varz_subscriptions":     1,
+	}
+
+	verifyCollector(url, cases, t)
+
+}
+
+// return fqName from parsing the Desc() field of a metric.
+func parseDesc(desc string) string {
+	// split on quotes.
+	return strings.Split(desc, "\"")[1]
+}
+
+func TestConnz(t *testing.T) {
+	s := pet.RunServer()
+	defer s.Shutdown()
+
+	url := fmt.Sprintf("http://localhost:%d", pet.MonitorPort)
+	// see if we get the same stats as the original monitor testing code.
+	// just for our monitoring_port
+
+	cases := map[string]float64{
+		"gnatsd_connz_total_connections": 0,
+		"gnatsd_varz_connections":        0,
+	}
+
+	verifyCollector(url, cases, t)
+
+	// Test with connections.
+	nc := pet.CreateClientConnSubscribeAndPublish(t)
+	defer nc.Close()
+}
+
+func verifyCollector(url string, cases map[string]float64, t *testing.T) {
+	// create a new collector.
+	servers := make([]*CollectedServer, 1)
+	servers[0] = &CollectedServer{
+		ID:  "id",
+		URL: fmt.Sprintf("http://localhost:%d", pet.MonitorPort),
+	}
+	coll := NewCollector("varz", servers)
+	// t.Fatalf("got body %s", coll)
+
+	// now collect the metrics
+	c := make(chan prometheus.Metric)
+	go coll.Collect(c)
+
+	for {
+		select {
+		case metric := <-c:
+			pb := &dto.Metric{}
+			metric.Write(pb)
+			gauge := pb.GetGauge()
+			val := gauge.GetValue()
+
+			name := parseDesc(metric.Desc().String())
+			fmt.Println("checking ", name)
+
+			expected, ok := cases[name]
+			if ok {
+				if val != expected {
+					t.Fatalf("Expected %s=%v, got %v", name, expected, val)
+				}
+			}
+		case <-time.After(10 * time.Millisecond):
+			return // pjm: there must be a smarter, safer, faster way to do this.
+		}
+	}
+}
