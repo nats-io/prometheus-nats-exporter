72e55d2006050f1d7d8efe300dad72364940babb
Colin Sullivan <colin@synadia.com>
2018-02-21 08:35:04 -0700

    Fix CI deployment

--- scripts/cov.sh
+++ scripts/cov.sh
@@ -1,17 +1,17 @@
 #!/bin/bash -e
 # Run from directory above via ./scripts/cov.sh
 
-rm -rf ./cov
-mkdir cov
-go test -v -covermode=atomic -coverprofile=./cov/collector.out ./collector
-go test -v -covermode=atomic -coverprofile=./cov/exporter.out ./exporter
-gocovmerge ./cov/*.out > acc.out
-rm -rf ./cov
+rm -rf /tmp/cov
+mkdir -p /tmp/cov
+go test -v -covermode=atomic -coverprofile=/tmp/cov/collector.out ./collector
+go test -v -covermode=atomic -coverprofile=/tmp/cov/exporter.out ./exporter
+gocovmerge /tmp/cov/*.out > /tmp/acc.out
+rm -rf /tmp/cov
 
 # If we have an arg, assume travis run and push to coveralls. Otherwise launch browser results
 if [[ -n $1 ]]; then
-    $HOME/gopath/bin/goveralls -coverprofile=acc.out -service travis-ci
-    rm -rf ./acc.out
+    $HOME/gopath/bin/goveralls -coverprofile=/tmp/acc.out -service travis-ci
+    rm -rf /tmp/acc.out
 else
-    go tool cover -html=acc.out
+    go tool cover -html=/tmp/acc.out
 fi
