a1ba4ae21a5656eb6efffa17502647afe3785241
Colin Sullivan <colin@synadia.com>
2018-03-20 13:13:05 -0600

    Changes for CNCF (#49)
    * Update license to Apache 2.0
    * Add OWNERS
    * Add CODE-OF-CONDUCT.md
    * Update vendored packages
    * Copyright updates

--- exporter/exporter.go
+++ exporter/exporter.go
@@ -1,4 +1,15 @@
-// Copyright 2017 Apcera Inc. All rights reserved.
+// Copyright 2017-2018 The NATS Authors
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
 package exporter
 
@@ -115,8 +126,8 @@ func (ne *NATSExporter) createCollector(endpoint string) {
 	}
 }
 
-// AddServer is an advanced API for Apcera's use; normally the NATS server
-// should be set through the options.  Adding more than one server will
+// AddServer is an advanced API; normally the NATS server should be set
+// through the options.  Adding more than one server will
 // violate Prometheus.io guidelines.
 func (ne *NATSExporter) AddServer(id, url string) error {
 	ne.Lock()
