63b5fb08045e00210cfdb7497abe0292552ca73a
Jaime Piña <jaime@synadia.com>
2020-10-13 17:06:51 -0700

    Refactor CI

--- scripts/cov.sh
+++ scripts/cov.sh
@@ -8,7 +8,7 @@ if [[ "$OSTYPE" != "linux-gnu" ]]; then
 	exit 0
 fi
 
-if [[ "$(go version)" != *"1.13"* ]]; then
+if [[ "$(go version)" != *"1.15"* ]]; then
 	echo "$(go version) detected, skipping upload overage"
 	exit 0
 fi
@@ -19,7 +19,7 @@ rm -rf /tmp/cov
 mkdir -p /tmp/cov
 go test -v -covermode=atomic -coverprofile=/tmp/cov/collector.out ./collector
 go test -v -covermode=atomic -coverprofile=/tmp/cov/exporter.out ./exporter
-$GOPATH/bin/gocovmerge /tmp/cov/*.out > coverage.txt
+$(go env GOPATH)/bin/gocovmerge /tmp/cov/*.out > coverage.txt
 rm -rf /tmp/cov
 
 # If we have an arg, assume travis run and push to coveralls. Otherwise launch
@@ -28,5 +28,5 @@ if [[ -n $1 ]]; then
 	echo "--- uploading test coverage ---"
 	bash <(curl -s https://codecov.io/bash)
 else
-    go tool cover -html=/tmp/acc.out
+    go tool cover -html=./coverage.txt
 fi
