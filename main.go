0a0b2fb67247f85b1e2e74512a367ea51fffd00d
Waldemar Quevedo <wally@nats.io>
2023-02-17 15:04:52 -0800

    Bump version
    Signed-off-by: Waldemar Quevedo <wally@nats.io>

--- main.go
+++ main.go
@@ -28,7 +28,7 @@ import (
 	"github.com/nats-io/prometheus-nats-exporter/exporter"
 )
 
-var version = "0.10.1"
+var version = "0.11.0"
 
 // parseServerIDAndURL parses the url argument the optional id for the server ID.
 func parseServerIDAndURL(urlArg string) (string, string, error) {
