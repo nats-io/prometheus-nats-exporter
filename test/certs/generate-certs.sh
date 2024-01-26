02f3723edef6169088a47359ac6beef929f677ea
Piotr Piotrowski <piotr@synadia.com>
2024-01-26 18:52:00 +0100

    Generate new test certs and overwrite default 30 day expiry
    Signed-off-by: Piotr Piotrowski <piotr@synadia.com>

--- test/certs/generate-certs.sh
+++ test/certs/generate-certs.sh
@@ -4,12 +4,12 @@
 openssl req -nodes -new -x509 -days 3650 -extensions v3_ca -keyout ca.key -out ca.pem -config openssl.cnf
 
 # Generate CSRs
-openssl req -nodes -new -keyout server.key -out server.csr -config openssl.cnf
-openssl req -nodes -new -keyout client.key -out client.csr -config openssl.cnf
+openssl req -nodes -new -days 3650 -keyout server.key -out server.csr -config openssl.cnf
+openssl req -nodes -new -days 3650 -keyout client.key -out client.csr -config openssl.cnf
 
 # Sign CSRs
-openssl x509 -req -extensions v3_req -extfile openssl.cnf -in server.csr -CA ca.pem -CAkey ca.key -CAcreateserial -out server.pem
-openssl x509 -req -extensions v3_req -extfile openssl.cnf -in client.csr -CA ca.pem -CAkey ca.key -CAcreateserial -out client.pem
+openssl x509 -req -days 3650 -extensions v3_req -extfile openssl.cnf -in server.csr -CA ca.pem -CAkey ca.key -CAcreateserial -out server.pem
+openssl x509 -req -days 3650 -extensions v3_req -extfile openssl.cnf -in client.csr -CA ca.pem -CAkey ca.key -CAcreateserial -out client.pem
 
 # These are test certs, cleanup extraneous files
 rm ca.key
