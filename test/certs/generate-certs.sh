2347bc4193633419e8bff4f5b310ae1bc3e9c654
Graham Reed <greed@hypervolt.co.uk>
2024-04-03 17:57:57 +0100

    Remove generated files before creating new ones
    As openssl will fail to overstrike write-protected files.

--- test/certs/generate-certs.sh
+++ test/certs/generate-certs.sh
@@ -2,6 +2,8 @@
 
 set -euo pipefail
 
+rm -f -- ca.pem client.key client.pem server.key server.pem
+
 # Generate CA
 openssl req -nodes -new -x509 -days 3650 -extensions v3_ca -keyout ca.key -out ca.pem -config openssl.cnf
 
