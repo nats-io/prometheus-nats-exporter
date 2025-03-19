f33bdc9b65c18bbe8809b1ae45ee0c2219638695
Tilak <tilak.raj94@gmail.com>
2025-03-20 01:08:48 +0530

    update description

--- collector/jsz.go
+++ collector/jsz.go
@@ -153,7 +153,7 @@ func newJszCollector(system, endpoint string, servers []*CollectedServer) promet
 		// jetstream_stream_usage
 		streamUsage: prometheus.NewDesc(
 			prometheus.BuildFQName(system, "stream", "usage"),
-			"Total used bytes from a stream",
+			"Represents the usage ratio of a JetStream stream as a fraction of its maximum configured storage.",
 			streamLabels,
 			nil,
 		),
